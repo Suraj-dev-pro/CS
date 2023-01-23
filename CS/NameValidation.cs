c310781759a81170feadc544b3f46505ffd94b5b
surajkarki <surazjungkarki@gmail.com>
2023-01-23 14:11:30 +0545

    minor changes

---
+++ CS/NameValidation.cs
@@ -0,0 +1,26 @@
+﻿/*C# program  that will read name from keyboard and 
+ * display it on screen.The Program should throw an exception 
+ * when length of name is more that 10 characters*/
+using System;
+
+class NameValidation
+{
+    static void Main()
+    {
+        Console.WriteLine("Please Enter you Name");
+        string name = Console.ReadLine();
+        try
+        {
+            if (name.Length > 10)
+            {
+                throw new Exception("Name cannot be of characters more tha 10");
+            }
+            Console.WriteLine("Your Name is " + name);
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+}
